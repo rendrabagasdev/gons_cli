6e6268ddb63d149e7913f688664fd51097e2f180
rendrabagasdev <rendrabagasdev@gmail.com>
2026-05-05 17:08:44 +0700

    fix: update repository URL to point to gons_framework

--- commands/new.go
+++ commands/new.go
@@ -20,15 +20,15 @@ var NewProjectCmd = &cobra.Command{
 }
 
 func createNewProject(name string) {
-	repoURL := "https://github.com/rendrabagasdev/gons"
-	
+	repoURL := "https://github.com/rendrabagasdev/gons_framework"
+
 	fmt.Printf("Creating new project: %s...\n", name)
 
 	// 1. Clone the repository
 	cloneCmd := exec.Command("git", "clone", repoURL, name)
 	cloneCmd.Stdout = os.Stdout
 	cloneCmd.Stderr = os.Stderr
-	
+
 	err := cloneCmd.Run()
 	if err != nil {
 		fmt.Printf("Error cloning repository: %v\n", err)
