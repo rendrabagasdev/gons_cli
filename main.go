97a622456e671a20d6371dcfc23a4b30076d4f07
rendrabagasdev <rendrabagasdev@gmail.com>
2026-05-05 17:04:08 +0700

    feat: implement dynamic versioning using build info and add version flag to root command

--- main.go
+++ main.go
@@ -3,17 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime/debug" // Import ini
 
 	"github.com/rendrabagasdev/gons/commands"
 	"github.com/spf13/cobra"
 )
 
-var version = "v1.0.0"
+var version = "dev" // Biarkan sebagai default untuk development lokal
 
 var rootCmd = &cobra.Command{
-	Use:     "gons",
-	Short:   "Gons CLI Framework",
-	Version: version,
+	Use:   "gons",
+	Short: "Gons CLI Framework",
+	// Kita tidak set Version di sini secara hardcoded lagi
 }
 
 func init() {
@@ -26,6 +27,16 @@ func init() {
 }
 
 func main() {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		if info.Main.Version != "" && info.Main.Version != "(devel)" {
+			rootCmd.Version = info.Main.Version
+		} else {
+			rootCmd.Version = version
+		}
+	}
+
+	rootCmd.Flags().BoolP("version", "v", false, "show version")
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
